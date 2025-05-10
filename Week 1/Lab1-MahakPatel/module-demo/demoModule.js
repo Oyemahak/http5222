ce27eab1f3fff7714e121dac176d82c272de8b35
Mahak Patel <mahakpateluiux@gmail.com>
2025-05-09 21:12:32 -0400

    Final Commit

--- Week 1/Lab1-MahakPatel/module-demo/demoModule.js
+++ Week 1/Lab1-MahakPatel/module-demo/demoModule.js
@@ -3,6 +3,8 @@ import { addItem, getItemCount } from './collectionModule.js';
 addItem("apples");
 addItem("oranges");
 addItem("bananas");
+addItem("pineapple");
+addItem("mango");
 
 console.log(`Current item count: ${getItemCount()}`);
 
